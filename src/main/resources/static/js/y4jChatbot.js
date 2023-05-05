7b151f506defae628cbd603700b60ae342af6d57
hormonee <mmmobl@naver.com>
2023-05-05 19:26:19 +0900

    230505 19:26 jm

--- src/main/resources/static/js/y4jChatbot.js
+++ src/main/resources/static/js/y4jChatbot.js
@@ -21,36 +21,73 @@ const sendMessage = async () => {
 
   chatInput.value = '';
 
-  //API Gateway 엔드포인트
-  const response = await fetch('https://ss7yz120b0.execute-api.ap-northeast-2.amazonaws.com/y4j/y4jChatbot', {
-//  const response = await fetch('http://localhost:3000/y4jChatbot', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      userMessages: userMessages,
-      assistantMessages: assistantMessages,
-    })
-  });
+  try {
+    //API Gateway 엔드포인트
+    const response = await fetch('https://ss7yz120b0.execute-api.ap-northeast-2.amazonaws.com/y4j/y4jChatbot', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        userMessages: userMessages,
+        assistantMessages: assistantMessages,
+      })
+    });
 
-  const data = await response.json();
-  document.getElementById('loader').style.display = "none";
+    const data = await response.json();
+    document.getElementById('loader').style.display = "none";
 
-  //assistantMessage 메세지 추가
-  assistantMessages.push(data.assistant);
+    //assistantMessage 메세지 추가
+    assistantMessages.push(data.assistant);
 
-  const botMsg = document.createElement('div');
-  botMsg.classList.add('chat-message');
-  botMsg.classList.add('bot-msg');
-  botMsg.innerHTML = `<p class='assistant'>${data.assistant}</p>`;
-  chatBox.appendChild(botMsg);
+    const botMsg = document.createElement('div');
+    botMsg.classList.add('chat-message');
+    botMsg.classList.add('bot-msg');
+    botMsg.innerHTML = `<p class='assistant'>${data.assistant}</p>`;
+    chatBox.appendChild(botMsg);
+
+  } catch (error) {
+    console.error(error);
+    document.getElementById('loader').style.display = "none";
+    
+    const botMsg = document.createElement('div');
+    botMsg.classList.add('chat-message');
+    botMsg.classList.add('bot-msg');
+    botMsg.innerHTML = `<p class='assistant'>네트워크 문제로 인해 응답에 실패했습니다.</p>`;
+    chatBox.appendChild(botMsg);
+  }
+
+  //   //API Gateway 엔드포인트
+  //   const response = await fetch('https://ss7yz120b0.execute-api.ap-northeast-2.amazonaws.com/y4j/y4jChatbot', {
+  // //  const response = await fetch('http://localhost:3000/y4jChatbot', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //       userMessages: userMessages,
+  //       assistantMessages: assistantMessages,
+  //     })
+  //   });
+
+  //   const data = await response.json();
+  //   document.getElementById('loader').style.display = "none";
+
+  //   //assistantMessage 메세지 추가
+  //   assistantMessages.push(data.assistant);
+
+  //   const botMsg = document.createElement('div');
+  //   botMsg.classList.add('chat-message');
+  //   botMsg.classList.add('bot-msg');
+  //   botMsg.innerHTML = `<p class='assistant'>${data.assistant}</p>`;
+  //   chatBox.appendChild(botMsg);
+
 };
 document.querySelector('.chat-input button').addEventListener('click', sendMessage);
 
 //질문 내용 입력 input 태그 내에서 엔터키 누를 시 디폴트 액션 방지 및 전송 처리
-$('.chat-input > input').keypress(function(e) {
-  if(e.keyCode == 13) {
+$('.chat-input > input').keypress(function (e) {
+  if (e.keyCode == 13) {
     e.preventDefault();
     sendMessage();
   }
